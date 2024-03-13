172c0c439a5d66164a9e85871e034689c0c7011e
Chrispine Oburu <chrizoburu65@gmail.com>
2024-03-13 12:50:57 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, surprise) {
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!`;
+        cards.push(message);
+    }
+    return cards;
+}
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(10)
